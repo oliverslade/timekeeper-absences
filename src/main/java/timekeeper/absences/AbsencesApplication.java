0a955051c6ebfdd080fdfff317ad4b6dfd5b658f
Oli Slade <olivergeorge.slade@gmail.com>
2019-08-16 17:43:25 +0100

    initial commit

---
+++ src/main/java/timekeeper/absences/AbsencesApplication.java
@@ -0,0 +1,13 @@
+package timekeeper.absences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsencesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/timekeeper/absences/AbsencesApplicationTests.java
@@ -0,0 +1,16 @@
+package timekeeper.absences;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbsencesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
