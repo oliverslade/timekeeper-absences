99c47b0b572be681aaec407657cdd1b655d0ceb4
Oli Slade <olivergeorge.slade@gmail.com>
2019-09-11 09:16:43 +0100

    #4: finishing off api documentation

--- src/main/java/timekeeper/absences/api/docs/AbsenceControllerDocs.java
+++ src/main/java/timekeeper/absences/api/docs/AbsenceControllerDocs.java
@@ -48,14 +48,49 @@ public interface AbsenceControllerDocs {
       @ApiParam(value = "The end of the period", required = true) LocalDate endOfPeriod,
       @ApiParam(value = "The user id to get absences for", required = true) long userId);
 
+  @ApiOperation(value = "Create a new absence", response = Absence.class)
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 409, message = "Absence already exists"),
+        @ApiResponse(code = 500, message = "Internal server error")
+      })
   @PostMapping("/create-absence")
   ResponseEntity<Absence> createAbsence(
-      long userId, AbsenceType type, LocalDate startDate, LocalDate endDate, String description);
+      @ApiParam(value = "The id of the user to add the absence to", required = true) long userId,
+      @ApiParam(value = "The type of absence to create", required = true) AbsenceType type,
+      @ApiParam(value = "The start date of the absence", required = true) LocalDate startDate,
+      @ApiParam(
+              value = "The end date of the absence (same as start date for illness)",
+              required = true)
+          LocalDate endDate,
+      @ApiParam(value = "Any additional information") String description);
 
+  @ApiOperation(value = "Approve an absence", response = Absence.class)
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 404, message = "Not found"),
+        @ApiResponse(code = 409, message = "Already approved"),
+        @ApiResponse(code = 500, message = "Internal server error")
+      })
   @PutMapping("/approve-absence")
-  ResponseEntity approveAbsence(long absenceId, long approverId);
+  ResponseEntity approveAbsence(
+      @ApiParam(value = "The id of the absence to be approved", required = true) long absenceId,
+      @ApiParam(value = "The user id of the person approving the absence", required = true)
+          long approverId);
 
+  @ApiOperation(value = "Update an absence", response = Absence.class)
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "OK"),
+        @ApiResponse(code = 404, message = "Not found"),
+        @ApiResponse(code = 500, message = "Internal server error")
+      })
   @PutMapping("/update-absence")
   ResponseEntity updateAbsence(
-      long absenceId, LocalDate startDate, LocalDate endDate, String description);
+      @ApiParam(value = "The id of the absence to update", required = true) long absenceId,
+      @ApiParam(value = "The start date of the absence", required = true) LocalDate startDate,
+      @ApiParam(value = "The end date of the absence", required = true) LocalDate endDate,
+      @ApiParam(value = "Any additional information") String description);
 }
